0de2fab6df7a58c619f3e8f52dcaf39a9a1d42fb
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-05-07 03:12:53 -0400

    First commit for video_to_ascii

--- src/img_to_ascii.py
+++ src/img_to_ascii.py
@@ -1,7 +1,7 @@
 import argparse
 import math
 import os
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 
 ASCII_CHARS = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@', 'M', 'W', '&', '$', 'X', 'A', 'O', 'K', 'B', '8']
 
@@ -20,6 +20,7 @@ def GetOutputImageName(image_path):
     input_file_name = os.path.basename(image_path)
     return input_file_name.split('.')[0] + '-ascii.' + input_file_name.split('.')[1]
 
+# IMPLEMENT Lanczos resize algorithm here.
 def ResizeImage(original_image, scale_factor):
     image_width, image_height = original_image.size
 
@@ -35,9 +36,7 @@ def GetCharacter(grey_val):
     char_index = math.floor(grey_val * len(ASCII_CHARS) / 256)
     return ASCII_CHARS[char_index]
 
-def ConvertToAscii(image_path):
-    original_image = Image.open(image_path)
-
+def ImageToAscii(original_image, image_path='input.jpg'):
     resized_image = ResizeImage(original_image, SCALE_FACTOR)
     resized_width, resized_height = resized_image.size
 
@@ -51,6 +50,7 @@ def ConvertToAscii(image_path):
     pixels = resized_image.load()
     drawer = ImageDraw.Draw(output_image)
 
+    # Speed up here using Multithreading/Multiprocessing
     for i in range(resized_height):
         for j in range(resized_width):
             r, g, b = pixels[j, i]
@@ -61,15 +61,19 @@ def ConvertToAscii(image_path):
             drawer.text(
                 (FONT_WIDTH * j, FONT_HEIGHT * i),
                 ascii_char,
-                fill=(r, g, b, 255)
+                fill=(r, g, b)
             )
         output_text.write('\n')
-
+    
     output_image.save(GetOutputImageName(image_path))
+
+def ConvertToAscii(image_path):
+    original_image = Image.open(image_path)
+    ImageToAscii(original_image, image_path)
 
 if __name__ == '__main__':
     # Parse the command line arguments
-    parser = argparse.ArgumentParser(description='Convert an image to ASCII art.')
+    parser = argparse.ArgumentParser(description='Convert an image to ASCII.')
     parser.add_argument('image_path', type=str, help='the path to the image file')
     args = parser.parse_args()
 
--- src/vid_to_ascii.py
+++ src/vid_to_ascii.py
@@ -1,7 +1,19 @@
-# IMPORT LIBRARIES
+import argparse
+import math
+import os
+from PIL import Image, ImageDraw
+import cv2
+import moviepy.editor as mp
 
-# calls img_to_ascii
 
-# Loads a video from Path
+def ConvertToAscii(video_path):
+    print('Hello, World2!')
 
-# Interacts with Command Line
+if __name__ == '__main__':
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description='Convert an video to ASCII.')
+    parser.add_argument('video_path', type=str, help='the path to the video file')
+    args = parser.parse_args()
+
+    # Convert the image to ASCII art
+    ConvertToAscii(args.video_path)
