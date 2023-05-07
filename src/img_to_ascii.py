9cd4dc01cd59df903856d0c10500a00f4897b6d1
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-05-07 01:32:44 -0400

    get new file names
    Implemented functions to get the names for the two output files, one for the text and one for the image file

--- src/img_to_ascii.py
+++ src/img_to_ascii.py
@@ -1,9 +1,17 @@
 import argparse
 import math
+import os
 from PIL import Image, ImageDraw, ImageFont
 
 ASCII_CHARS = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@', 'M', 'W', '&', '$', 'X', 'A', 'O', 'K', 'B', '8']
 
+def GetOutputTextName(image_path):
+    input_file_name = os.path.basename(image_path)
+    return input_file_name.split('.')[0] + '-ascii.txt'
+
+def GetOutputImageName(image_path):
+    input_file_name = os.path.basename(image_path)
+    return input_file_name.split('.')[0] + '-ascii.' + input_file_name.split('.')[1]
 
 def ResizeImage(original_image):
     return 0
@@ -13,9 +21,19 @@ def ConvertToAscii(image_path):
     original_width, original_height = original_image.size
 
     resized_image = ResizeImage(original_image)
-    resized_width, resized_height = resized_image.size
+    # resized_width, resized_height = resized_image.size
 
-    pass
+    print(GetOutputTextName(image_path))
+    print(GetOutputImageName(image_path))
+
+    # output_text = open(GetOutputTextName(image_path), 'w')
+    # output_image = 0
+
+    # drawer = ImageDraw.Draw(output_image)
+
+    # # DRAW
+
+    # output_image.save(GetOutputImageName(image_path))
 
 
 
