f7dd6fa2632c503a14b8eb4f26f1eb08699a47a4
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-05-07 01:55:35 -0400

    Implemented ascii text
    Converts to ascii_text and scales as expected. But there are still issues with the output ascii image regarding scaling and ascii char output

--- src/img_to_ascii.py
+++ src/img_to_ascii.py
@@ -8,8 +8,8 @@ ASCII_CHARS = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@', 'M', 'W', '&', 
 FONT_WIDTH = 1
 FONT_HEIGHT = 2
 
-FONT_RATIO = int(FONT_WIDTH / FONT_HEIGHT)
-SCALE_FACTOR = 0.2
+FONT_RATIO = FONT_WIDTH / FONT_HEIGHT
+SCALE_FACTOR = 2
 
 def GetOutputTextName(image_path):
     input_file_name = os.path.basename(image_path)
@@ -30,25 +30,41 @@ def ResizeImage(original_image, scale_factor):
         Image.NEAREST
     )
 
-def ConvertToAscii(image_path):
-    
+def GetCharacter(grey_val):
+    char_index = math.floor(grey_val * len(ASCII_CHARS) / 256)
+    return ASCII_CHARS[char_index]
 
+def ConvertToAscii(image_path):
     original_image = Image.open(image_path)
-    original_width, original_height = original_image.size
 
     resized_image = ResizeImage(original_image, SCALE_FACTOR)
     resized_width, resized_height = resized_image.size
 
     output_text = open(GetOutputTextName(image_path), 'w')
-    output_image = 0
+    output_image = Image.new(
+        'RGB',
+        (FONT_WIDTH * resized_width, FONT_HEIGHT * resized_height),
+        color=(0, 0, 0)
+    )
 
+    pixels = resized_image.load()
     drawer = ImageDraw.Draw(output_image)
 
-    # DRAW
+    for i in range(resized_height):
+        for j in range(resized_width):
+            r, g, b = pixels[j, i]
+
+            ascii_char = GetCharacter(int((r + g + b) / 3))
+
+            output_text.write(ascii_char)
+            drawer.text(
+                (FONT_WIDTH * j, FONT_HEIGHT * i),
+                ascii_char,
+                fill=(r, g, b, 255)
+            )
+        output_text.write('\n')
 
     output_image.save(GetOutputImageName(image_path))
-
-
 
 if __name__ == '__main__':
     # Parse the command line arguments
