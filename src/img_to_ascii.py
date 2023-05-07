8da0e1a05515b3439294612738010e6fc2129288
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2023-05-07 01:42:15 -0400

    PIL Resize
    Implemented Resize using the PIL built in resize function. Also added FONT_RATIO and SCALE_FACTOR constants. SCALE_FACTOR is for resizing, which determines how much the resize should be, and FONT_RATIO is to account for the fact that font characters do not have the same width and height. This depends on font.

--- src/img_to_ascii.py
+++ src/img_to_ascii.py
@@ -5,6 +5,12 @@ from PIL import Image, ImageDraw, ImageFont
 
 ASCII_CHARS = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@', 'M', 'W', '&', '$', 'X', 'A', 'O', 'K', 'B', '8']
 
+FONT_WIDTH = 1
+FONT_HEIGHT = 2
+
+FONT_RATIO = int(FONT_WIDTH / FONT_HEIGHT)
+SCALE_FACTOR = 0.2
+
 def GetOutputTextName(image_path):
     input_file_name = os.path.basename(image_path)
     return input_file_name.split('.')[0] + '-ascii.txt'
@@ -13,27 +19,34 @@ def GetOutputImageName(image_path):
     input_file_name = os.path.basename(image_path)
     return input_file_name.split('.')[0] + '-ascii.' + input_file_name.split('.')[1]
 
-def ResizeImage(original_image):
-    return 0
+def ResizeImage(original_image, scale_factor):
+    image_width, image_height = original_image.size
+
+    return original_image.resize(
+        (
+            int(scale_factor * image_width),
+            int(scale_factor * FONT_RATIO * image_height)
+        ),
+        Image.NEAREST
+    )
 
 def ConvertToAscii(image_path):
+    
+
     original_image = Image.open(image_path)
     original_width, original_height = original_image.size
 
-    resized_image = ResizeImage(original_image)
-    # resized_width, resized_height = resized_image.size
+    resized_image = ResizeImage(original_image, SCALE_FACTOR)
+    resized_width, resized_height = resized_image.size
 
-    print(GetOutputTextName(image_path))
-    print(GetOutputImageName(image_path))
+    output_text = open(GetOutputTextName(image_path), 'w')
+    output_image = 0
 
-    # output_text = open(GetOutputTextName(image_path), 'w')
-    # output_image = 0
+    drawer = ImageDraw.Draw(output_image)
 
-    # drawer = ImageDraw.Draw(output_image)
+    # DRAW
 
-    # # DRAW
-
-    # output_image.save(GetOutputImageName(image_path))
+    output_image.save(GetOutputImageName(image_path))
 
 
 
